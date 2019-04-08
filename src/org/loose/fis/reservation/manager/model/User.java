36fa4822be05824ca18ff1efabb3617fb0733ae1
ciucur codrut <ciucurcasian6@gmail.com>
2019-04-08 15:19:56 +0300

    Merge branch ‘master’ into cleanup

--- src/org/loose/fis/reservation/manager/model/User.java
+++ src/org/loose/fis/reservation/manager/model/User.java
@@ -6,8 +6,12 @@ import java.util.UUID;
 public class User {
     private String id;
     private String firstName;
-    private String lastName;
+
+    // optional field (only if users will require it)
+
     private int age;
+
+    // field that says if the user owns a company that you can get reservations to
     private boolean isOwner;
 
     public User(String firstName, String lastName, int age) {
